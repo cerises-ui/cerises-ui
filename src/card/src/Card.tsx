acb0596965f372d7ab4191cd260fa977e18e0598
Yorksh1re <ezreeeeal@gmail.com>
2022-03-23 23:29:59 +0800

    fix: card style

--- src/card/src/Card.tsx
+++ src/card/src/Card.tsx
@@ -1,7 +1,12 @@
 export default (props) => {
   return (
     <>
-      <div class="w-full p-6 border-1 space-y-6 rounded">
+      <div
+        class={
+          'w-full p-6 border-1 space-y-6 rounded' +
+          (props.class ? ' ' + props.class : '')
+        }
+      >
         <span class="text-xl">{props.title}</span>
         <div>{props.children}</div>
       </div>
