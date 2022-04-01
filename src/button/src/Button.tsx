392891433e6cde2a62f87d3c8653e775a3e16f1e
Yorksh1re <ezreeeeal@gmail.com>
2022-04-01 23:48:08 +0800

    fix: button

--- src/button/src/Button.tsx
+++ src/button/src/Button.tsx
@@ -3,11 +3,13 @@ import type { ButtonProps } from './interface';
 import '../styles/index.scss';
 
 export default (props: ButtonProps) => {
+  console.log(props);
   return (
     <>
       <button
-        disabled={props.disabled}
         class={`c-button${props.class ? ' ' + props.class : ''}`}
+        size={props.size}
+        disabled={props.disabled}
       >
         {props.children}
       </button>
